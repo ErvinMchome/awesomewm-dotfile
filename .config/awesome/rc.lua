6a553dbe63ad68dec42c0446814aee42298584f0
ervinmckeats <animator4sr@gmail.com>
2022-06-18 20:54:13 +0300

    .

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -83,14 +83,14 @@ tag.connect_signal("request::default_layouts", function()
     awful.layout.append_default_layouts({
         awful.layout.suit.floating,
         awful.layout.suit.tile.left,
-        awful.layout.suit.max,
-        -- awful.layout.suit.tile,
-        -- awful.layout.suit.tile.bottom,
+        awful.layout.suit.tile,
+        awful.layout.suit.tile.bottom,
         -- awful.layout.suit.tile.top,
         -- awful.layout.suit.fair,
         -- awful.layout.suit.fair.horizontal,
         -- awful.layout.suit.spiral,
         -- awful.layout.suit.spiral.dwindle,
+        -- awful.layout.suit.max,
         -- awful.layout.suit.max.fullscreen,
         -- awful.layout.suit.magnifier,
         -- awful.layout.suit.corner.nw,
@@ -306,13 +306,13 @@ awful.keyboard.append_global_keybindings({
 
 -- Focus related keybindings
 awful.keyboard.append_global_keybindings({
-    awful.key({ modkey,           }, "j",
+    awful.key({ alt,           }, "Left",
         function ()
             awful.client.focus.byidx( 1)
         end,
         {description = "focus next by index", group = "client"}
     ),
-    awful.key({ modkey,           }, "k",
+    awful.key({ alt,           }, "Right",
         function ()
             awful.client.focus.byidx(-1)
         end,
