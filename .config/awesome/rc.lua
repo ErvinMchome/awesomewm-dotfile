1281a8144f5783ce68df34bf0681539ba7f079e4
Ervin McKeats <83841376+ervinmckeats@users.noreply.github.com>
2022-07-21 23:49:19 +0300

    Add files via upload

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -40,6 +40,7 @@ beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.useless_gap = 2.9
 -- This is used later as the default terminal and editor to run.
 terminal = "kitty"
+browser="google-chrome-stable"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
@@ -65,8 +66,17 @@ myawesomemenu = {
    { "quit", function() awesome.quit() end },
 }
 
+
+internet = {
+    {"Firefox", "firefox"},
+    {"Google Chrome", "google-chorme-stable"}
+}
+
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
+                                    { "keyboard", "florence" },
+                                    { "VLC", "vlc"} ,
+                                    { "internet", internet },
+                                    {"File Manager", "nemo" }
                                   }
                         })
 
@@ -82,15 +92,15 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 tag.connect_signal("request::default_layouts", function()
     awful.layout.append_default_layouts({
         awful.layout.suit.floating,
+        awful.layout.suit.max,
         awful.layout.suit.tile.left,
-        awful.layout.suit.tile,
+        -- awful.layout.suit.tile,
         awful.layout.suit.tile.bottom,
         -- awful.layout.suit.tile.top,
         -- awful.layout.suit.fair,
         -- awful.layout.suit.fair.horizontal,
         -- awful.layout.suit.spiral,
         -- awful.layout.suit.spiral.dwindle,
-        -- awful.layout.suit.max,
         -- awful.layout.suit.max.fullscreen,
         -- awful.layout.suit.magnifier,
         -- awful.layout.suit.corner.nw,
